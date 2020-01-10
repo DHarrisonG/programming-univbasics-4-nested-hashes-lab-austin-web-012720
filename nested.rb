aa1c5eb2d68bf1b0ef6aaf11164868ea8479ad82
Doug Harrison <dougharrisong@gmail.com>
2020-01-10 01:57:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,9 +80,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+    programmer_hash
 end
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-programmer_hash
+
 
 def changing_alan
 	programmer_hash = 
